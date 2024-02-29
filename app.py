3e8f304991eb0012248c2a9b9760e493eb43919b
nmalvankar <nmalvankar@gmail.com>
2024-02-29 12:34:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, base64
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'FLAG_HELLO_99'
+    hostname = request.host.split(':')[0]
+    hello = "SEVMTE9fV09STEQ="
+    aloha = "QUxPSEFfR1JFRVRJTkc="
+    if 'aloha' in hostname:
+        aloha_byte = base64.b64decode(encoded_string)
+        return aloha_byte.decode('utf-8')
+    else: 
+        hello_byte = base64.b64decode(encoded_string)
+        return hello_byte.decode('utf-8')
 
 app.run(host="0.0.0.0", port=8080)
